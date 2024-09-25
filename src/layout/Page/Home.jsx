0edc5a18025e927724b7b53d39aad77a894830b8
Naim Khan <naimkhan01856@gmail.com>
2024-09-25 17:00:45 +0600

    earbuds section done

--- src/layout/Page/Home.jsx
+++ src/layout/Page/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../../components/Home/Banner';
 import Neckband from '../../components/Home/Neckband';
+import Earbuds from '../../components/Home/Earbuds';
 
 const Home = () => {
     return (
         <>
             <Banner/>
             <Neckband/>
+            <Earbuds/>
         </>
     );
 };
