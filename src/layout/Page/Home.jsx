12e6e447a39c2cecea4946e9d9afe1d71e7cdf08
Naim Khan <naimkhan01856@gmail.com>
2024-09-25 17:09:34 +0600

    headphone section done

--- src/layout/Page/Home.jsx
+++ src/layout/Page/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../../components/Home/Banner';
 import Neckband from '../../components/Home/Neckband';
 import Earbuds from '../../components/Home/Earbuds';
+import HeadPhone from '../../components/Home/HeadPhone';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner/>
             <Neckband/>
             <Earbuds/>
+            <HeadPhone/>
         </>
     );
 };
