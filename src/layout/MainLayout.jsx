9775551a91e2bbbaee9e8ad0707ebf8377484b15
Naim Khan <naimkhan01856@gmail.com>
2024-09-25 14:14:41 +0600

    Navbar done

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../shared/Navbar";
 
 
 const MainLayout = () => {
   return (
     <>
       <div>
+        <Navbar/>
         <Outlet />
       </div>
     </>
